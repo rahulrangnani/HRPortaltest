28ab9853d8eb4302fa4513533684fd30384a672b
Rahul Rangnani <43803094+rahulrangnani@users.noreply.github.com>
2025-12-09 14:15:54 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,4 @@
 "use client";
-import Script from "next/script";
 
 import { useEffect } from 'react';
 import "./globals.css";
@@ -16,9 +15,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en" data-theme="corporate" className="h-full">
-      <head>
-        <Script src="https://subtle-druid-430b16.netlify.app/codemate-badge.js" strategy="lazyOnload" />
-      </head>
       <body className="antialiased">
         <div className="flex flex-col min-h-screen bg-base-200">
           <Header />
